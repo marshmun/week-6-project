78f485380cdadcb64319b5455faab40d325ff561
Marshall Munoz <marshmun@gmail.com>
2017-06-16 08:10:07 -0500

    linking api to server, and added search box

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const userInfo = " id=8538a1744a7fdaa59981232897501e04";
+const clientUse = "https://api.soundcloud.com/tracks?client"
+
 /*
   Here is a guide for the steps you could take:
 */
@@ -15,3 +18,25 @@
 
 
 // 5. Create a way to listen for a click that will play the song in the audio play
+function music() {
+    fetch("https://api.soundcloud.com/tracks?client_id=8538a1744a7fdaa59981232897501e04")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var customers = data.results;
+            for (var i = 0; i < customers.length; i++) {
+                console.log("customer : ", customers[i])
+                thisStuf(customers[i])
+            }
+
+        })
+}
+function thisStuf(data) {
+
+    function createProfileWrapper() {
+        var profileEverything = document.createElement('div');
+        customers.appendChild(profileEverything);
+    }
+}
+music()
